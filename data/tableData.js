15a0745e404e5ad46420f3532ea3465ec4a7b198
Fred M <farzad.moghbel@gmail.com>
2019-08-18 17:01:37 -0400

    the initial data modules are done

---
+++ data/tableData.js
@@ -0,0 +1,11 @@
+var tableArrays = [
+    {
+        ID: "2345",
+        Name: "David",
+        Email: "david@example.com",
+        Phone: 000-000-000
+    }
+];
+
+module.exports = tableArrays;
+
---
+++ data/waitinglistData.js
@@ -0,0 +1,10 @@
+var waitingArray = [
+    {
+        customerName: "Sala",
+        customerEmail: "sala@example.com",
+        phoneNumber: "000-000-000",
+        customerID: "Sala123"
+    }
+];
+
+module.exports = waitingArray;
